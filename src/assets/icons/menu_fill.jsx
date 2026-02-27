d2d0b1a97edbf73af4373805f1101a567ab02c7a
Laiba-compilex <laiba.yousaf@compilex.it.com>
2026-02-27 09:07:28 +0500

    icon files name fixed

--- src/assets/icons/menu_fill.jsx
+++ src/assets/icons/menu_fill.jsx
@@ -1,5 +1,21 @@
 export const MenuFill = () => (
-    <svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M17 12C17.5523 12 18 12.4477 18 13C18 13.5523 17.5523 14 17 14H1C0.44772 14 0 13.5523 0 13C0 12.4477 0.44772 12 1 12H17ZM17 6C17.5523 6 18 6.4477 18 7C18 7.5523 17.5523 8 17 8H1C0.44772 8 0 7.5523 0 7C0 6.4477 0.44772 6 1 6H17ZM17 0C17.5523 0 18 0.44772 18 1C18 1.55228 17.5523 2 17 2H1C0.44772 2 0 1.55228 0 1C0 0.44772 0.44772 0 1 0H17Z" fill="black" />
-    </svg>
+    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M23.3333 21.3335C23.7015 21.3335 24 21.632 24 22.0002C24 22.3684 23.7015 22.6668 23.3333 22.6668H12.6667C12.2985 22.6668 12 22.3684 12 22.0002C12 21.632 12.2985 21.3335 12.6667 21.3335H23.3333ZM23.3333 17.3335C23.7015 17.3335 24 17.632 24 18.0002C24 18.3684 23.7015 18.6668 23.3333 18.6668H12.6667C12.2985 18.6668 12 18.3684 12 18.0002C12 17.632 12.2985 17.3335 12.6667 17.3335H23.3333ZM23.3333 13.3335C23.7015 13.3335 24 13.632 24 14.0002C24 14.3683 23.7015 14.6668 23.3333 14.6668H12.6667C12.2985 14.6668 12 14.3683 12 14.0002C12 13.632 12.2985 13.3335 12.6667 13.3335H23.3333Z" fill="url(#paint0_linear_331_3316)"/>
+<circle cx="18" cy="18" r="17.5" stroke="url(#paint1_linear_331_3316)"/>
+<defs>
+<linearGradient id="paint0_linear_331_3316" x1="16.8581" y1="13.3335" x2="17.239" y2="24.9539" gradientUnits="userSpaceOnUse">
+<stop stop-color="#EDF9FF"/>
+<stop offset="0.4" stop-color="#5787FF"/>
+<stop offset="0.65" stop-color="#229FFF"/>
+<stop offset="0.85" stop-color="#3561FC"/>
+</linearGradient>
+<linearGradient id="paint1_linear_331_3316" x1="18.7643" y1="2.24034e-09" x2="20.1433" y2="35.9471" gradientUnits="userSpaceOnUse">
+<stop stop-color="#EDF9FF"/>
+<stop offset="0.4" stop-color="#5787FF"/>
+<stop offset="0.65" stop-color="#229FFF"/>
+<stop offset="0.85" stop-color="#3561FC"/>
+</linearGradient>
+</defs>
+</svg>
+
 );
