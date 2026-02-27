f0acb86e4e3eef4a823c6f78851d5476fe78f918
Laiba-compilex <laiba.yousaf@compilex.it.com>
2026-02-27 09:01:51 +0500

    icons syntax fixed

--- src/assets/icons/arrowRight.js
+++ src/assets/icons/arrowRight.js
@@ -1,4 +1,4 @@
-export const ArrowRightIcon = (
+export const ArrowRightIcon = () => (
     <svg width="5" height="9" viewBox="0 0 5 9" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 0.500955C0 0.0555252 0.5385 -0.167515 0.8535 0.147445L4.293 3.58687C4.6834 3.97737 4.6834 4.61046 4.293 5.00096L0.8535 8.44036C0.5386 8.75527 0.0001 8.53227 0 8.08687V0.500955Z" fill="black" />
     </svg>
--- src/assets/icons/lineArrowLineBottom.js
+++ src/assets/icons/lineArrowLineBottom.js
@@ -1,4 +1,4 @@
-export const LineArrowLineBottomIcon = (
+export const LineArrowLineBottomIcon = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1.55359 7.17847L11.4318 17.0566C11.7456 17.3705 12.2544 17.3705 12.5682 17.0566L22.4464 7.17847" stroke="#050E1F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
